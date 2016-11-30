4511b20c831d47013fa1b1d951b3ad9437b6cdbf
Javier Cienfuegos <cienfuegosj@berea.edu>
2016-11-30 01:28:47 -0500

    BET

--- cienfuegosj-cvetanovskaa-FP/GUIApp.py
+++ cienfuegosj-cvetanovskaa-FP/GUIApp.py
@@ -5,10 +5,12 @@ Description: Tkinter objects will defined here
 '''
 
 from Tkinter import *
+from pos import pos
+from Stack import Stack
 import tkMessageBox
 import copy
 import time
-import pos
+
 
 class FileApp:
 
@@ -16,8 +18,6 @@ class FileApp:
 
         self.root = master
         self.f = None
-
-        '''StringVar objects defined here for labels'''
 
         lbl_filename = StringVar()
         lbl_filename.set("Enter filename: ")
@@ -90,7 +90,7 @@ class FileApp:
         for line in matrix:
             for char in line:
                 if char == 'M':
-                    initialpos = pos.pos(matrix.index(line), line.index(char))
+                    initialpos = pos(matrix.index(line), line.index(char))
                 else:
                     continue
         # ------------------------------------------------------------------------------
@@ -101,9 +101,21 @@ class MainApp:
 
     def __init__(self, master, content, initialpos):
 
-        self.root = master
-        self.content = content
+        # Functionality Setup
+        self.mappings = {}
+        self.currentState = pos()
+        self.nextState = pos()
+        self.treasures = {}
+        self.stackObject = Stack()
+        self.matrix = content
         self.initialpos = initialpos
+
+        # GUI Setup
+        self.root = master
+        self.canvaswidth = 700
+        self.canvasheight = 400
+        self.row = len(content)
+        self.col = len(content[0])
 
         self.window = PanedWindow(master, orient = VERTICAL,
                     bd=4,
@@ -113,22 +125,149 @@ class MainApp:
         self.mainpane = Canvas(self.window,
                         bg="white",
                         bd=3,
-                        width=700,
-                        height=400
+                        width=self.canvaswidth,
+                        height=self.canvasheight
                         )
-        coord = 10, 50, 40, 80
-        oval = self.mainpane.create_oval(coord, fill="blue")
+
+        self.draw()
         self.mainpane.pack(side=TOP)
 
 
-        self.statisticspane = PanedWindow(self.window, orient=VERTICAL,
-                            bg="grey",
-                            bd=3,
-                            borderwidth=4,
-                            width=700,
-                            height=100
-                            )
-        self.statisticspane.pack(side=BOTTOM)
+
+    def draw(self):
+
+        xscalingfactor = self.canvaswidth/self.col-5
+        yscalingfactor = self.canvasheight/self.row-5
+        xscalingfactor/=2
+
+        x1 = 10
+        y1 = 10
+        x2 = 10 + xscalingfactor
+        y2 = 10 + yscalingfactor
+
+        for i in range(self.row):
+            for j in range(self.col):
+                if self.matrix[i][j] == "M":
+                    self.mappings[(i,j)] = (x1,y1,x2,y2)
+                    self.mainpane.create_oval(x1,y1,x2,y2, fill="blue", outline="blue")
+                elif self.matrix[i][j] == "W":
+                    self.mappings[(i, j)] = (x1, y1, x2, y2)
+                    self.mainpane.create_rectangle(x1,y1,x2,y2, outline="red", fill="red")
+                elif self.matrix[i][j] == "T":
+                    self.mappings[(i, j)] = (x1, y1, x2, y2)
+                    self.mainpane.create_rectangle(x1,y1,x2,y2, fill="#fff517", outline="#fff517")
+                else:
+                    self.mappings[(i, j)] = (x1, y1, x2, y2)
+                    self.mainpane.create_oval(x1,y1,x2,y2, fill="white", outline="white")
+                x1=x1+xscalingfactor+10
+                x2=x2+xscalingfactor+10
+            y1=y1+yscalingfactor+5
+            y2=y2+yscalingfactor+5
+            x1 = 10
+            x2 = 10+xscalingfactor
+
+        self.gamePlay()
+
+    def checkTreasure(self):
+        '''
+        No pre conditions are made for this method.
+        :return: notifies the user that we found treasure and we store the steps.
+        '''
+
+        if self.matrix[self.nextState.row][self.nextState.col] == "T":
+            # If the next position is treasure then map it to the list of directions it took to get there
+            tkMessageBox.showinfo("Treasure Info", "Treasure has been found!")
+            x = copy.deepcopy(self.stackObject.items)
+            position = pos(self.nextState.row, self.nextState.col)
+            self.treasures[position] = x
+
+    def checkPos(self):
+
+        if self.matrix[self.currentState.row - 1][self.currentState.col] == "." and \
+                        self.matrix[self.currentState.row - 1][self.currentState.col] != "B":
+            self.stackObject.push(0)
+            self.currentState.row -= 1
+            self.nextState.row = self.currentState.row - 1
+            self.nextState.col = self.currentState.col
+            self.matrix[self.currentState.row][self.currentState.col] = "B"
+            self.mainpane.create_oval(self.mappings[(self.currentState.row,self.currentState.col)][0],self.mappings[(self.currentState.row,self.currentState.col)][1],self.mappings[(self.currentState.row,self.currentState.col)][2],self.mappings[(self.currentState.row,self.currentState.col)][3], fill="blue")
+            self.checkTreasure()
+        elif self.matrix[self.currentState.row][self.currentState.col + 1] == "." and \
+                        self.matrix[self.currentState.row][self.currentState.col + 1] != "B":
+            self.stackObject.push(1)
+            self.currentState.col = self.currentState.col + 1
+            self.nextState.row = self.currentState.row
+            self.nextState.col = self.currentState.col + 1
+            self.matrix[self.currentState.row][self.currentState.col] = "B"
+            self.mainpane.create_oval(self.mappings[(self.currentState.row,self.currentState.col)][0],self.mappings[(self.currentState.row,self.currentState.col)][1],self.mappings[(self.currentState.row,self.currentState.col)][2],self.mappings[(self.currentState.row,self.currentState.col)][3], fill="blue")
+            self.checkTreasure()
+        elif self.matrix[self.currentState.row][self.currentState.col - 1] == "." and \
+                        self.matrix[self.currentState.row][self.currentState.col - 1] != "B":
+            self.stackObject.push(3)
+            self.currentState.col = self.currentState.col - 1
+            self.nextState.row = self.currentState.row
+            self.nextState.col = self.currentState.col - 1
+            self.matrix[self.currentState.row][self.currentState.col] = "B"
+            self.mainpane.create_oval(self.mappings[(self.currentState.row,self.currentState.col)][0],self.mappings[(self.currentState.row,self.currentState.col)][1],self.mappings[(self.currentState.row,self.currentState.col)][2],self.mappings[(self.currentState.row,self.currentState.col)][3], fill="blue")
+            self.checkTreasure()
+        elif self.matrix[self.currentState.row + 1][self.currentState.col] == '.' and \
+                        self.matrix[self.currentState.row + 1][self.currentState.col] != "B":
+            self.stackObject.push(2)
+            self.nextState.row = self.currentState.row + 1
+            self.nextState.col = self.currentState.col
+            self.currentState.row = self.currentState.row + 1
+            self.matrix[self.currentState.row][self.currentState.col] = "B"
+            self.mainpane.create_oval(self.mappings[(self.currentState.row,self.currentState.col)][0],self.mappings[(self.currentState.row,self.currentState.col)][1],self.mappings[(self.currentState.row,self.currentState.col)][2],self.mappings[(self.currentState.row,self.currentState.col)][3], fill="blue")
+            self.checkTreasure()
+        else:
+            return 0
+
+    def gamePlay(self):
+        '''
+        :param initpos: Takes in the initial position as a position object.
+        :return: Allows flow of the function as long as we are able to move throughout the map.
+        '''
+
+        self.currentState.col = self.initialpos.col
+        self.currentState.row = self.initialpos.row
+
+        while True:
+            move = self.checkPos()
+            if move == 0:  # Condition if there is no move.
+
+                if self.stackObject.size() == 0:
+
+                    print("Treasure Path List: ")
+                    for key in list(self.treasures.keys()):
+                        print(self.treasures[key])
+
+                    print("That's all folks!")
+                    break
+
+                else:
+                    item = self.stackObject.pop()
+
+                    # ---------Backtrack Process------------
+                    # 0 represents that we went NORTH, so we need to go down, so row increase
+                    # 1 represents that we went EAST, so we need
+                    if item == 0:
+                        self.currentState.row += 1
+                    elif item == 1:
+                        self.currentState.col -= 1
+                    elif item == 3:
+                        self.currentState.col += 1
+                    else:
+                        self.currentState.row -= 1
+            else:
+                continue
+
+
+
+
+
+
+
+
 
 
 
--- cienfuegosj-cvetanovskaa-FP/README.md
+++ cienfuegosj-cvetanovskaa-FP/README.md
@@ -5,5 +5,3 @@ game.py
     -> Create one window that allows us to ask for the user to select a file
     -> After a valid file has been selected, then we open a widget/window (Unsure about the technical word)
     -> Run the Game Loop :)
-
-
